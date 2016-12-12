da9a53c8a9f4b62f7b7c97a313af70e800cf27d8
Alexander Groß <agross@NRSMAC02T.local>
2016-12-12 09:52:14 +0100

    removeDir

--- 1.1/README.md
+++
@@ -1,2 +0,0 @@
-# c-IO_Library
-Test Repos
--- 1.1/io_library.h
+++
@@ -1,7 +0,0 @@
-#ifndef _IO_LIBRARY_H
-#define _IO_LIBRARY_H
-
-int IO_getChar(char* character);
-int IO_getString(unsigned int length, char* character);
-
-#endif
